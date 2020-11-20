d2fe27fbb2b3c3885d6c46c3c37ff4bb13a1d89b
kiranshivale <kiran.shivale@fideltech.com>
2020-11-20 16:07:52 +0530

    First Commit

---
+++ src/main/java/com/vhm/qa/base/TestBase.java
@@ -0,0 +1,53 @@
+package com.vhm.qa.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.vhm.qa.utils.TestUtil;
+
+public class TestBase {
+
+	public static WebDriver driver;
+	public static Properties prop;
+
+	public TestBase() {
+		try {
+			prop = new Properties();
+			final FileInputStream ip = new FileInputStream("D:\\Fidel\\Selenium Projects\\AutomationDemo-master\\AutomationDemo-master\\src\\main\\java\\com\\vhm\\qa\\config\\config.properties");
+			prop.load(ip);
+
+		} catch (final FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void initialization(){
+			final String browserName = prop.getProperty("Browser");
+
+			if(browserName.equals("chrome")) {
+				System.setProperty("webdriver.chrome.driver",
+						"C:\\\\Users\\\\Nilu\\\\Desktop\\\\SeleniumSW\\\\chromedriver_win32\\\\chromedriver.exe");
+				driver = new ChromeDriver();
+			}
+
+			driver.manage().window().maximize();
+			driver.manage().deleteAllCookies();
+
+			driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT,TimeUnit.SECONDS);
+
+			driver.get(prop.getProperty("url"));
+
+	}
+
+
+
+}
---
+++ src/main/java/com/vhm/qa/pages/DashboardPage.java
@@ -0,0 +1,56 @@
+package com.vhm.qa.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.vhm.qa.base.TestBase;
+
+public class DashboardPage extends TestBase {
+
+	// page factory (Obj Repo)
+	@FindBy(xpath = "//img[@alt='Village House']")
+	WebElement houseLogo;
+
+	//// p[contains(text(),"物件(部屋)")]
+	@FindBy(xpath = "//p[contains(text(),'物件(部屋)')]")
+	WebElement units;
+
+	@FindBy(xpath = "//a[@class='dropdown-toggle']")
+	WebElement userIcon;
+	
+	@FindBy(xpath = "//i[@class='fas fa-sign-out-alt mr-2']")
+	WebElement logOutIcon;
+	
+	@FindBy(xpath = "//a[@class='nav-link flag-link active']")
+	WebElement langugeSelctionIcon;
+	
+	@FindBy(xpath = "//a[@class='dropdown-item'] //img")
+	WebElement englishJpIcon;
+	
+	public DashboardPage() {
+		PageFactory.initElements(driver, this);
+	}
+
+	public boolean homeLogoShowing() {
+		return	houseLogo.isDisplayed();
+	}
+	
+	public LoginPage logoutUser() {
+		clickOnUserIcon();
+		logOutIcon.click();
+		return new LoginPage();
+	}
+	
+	public void clickOnUserIcon() {
+		userIcon.click();
+	}
+	
+	public void openLanguageList() {
+		langugeSelctionIcon.click();		
+	}
+	public void selectEngLang() {
+		openLanguageList();
+		englishJpIcon.click();
+	}
+}
